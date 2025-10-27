cff07a8182c1b40af8fe6ee80c419099b8c8b711
lukfu <fu.lukas99@gmail.com>
2025-10-27 15:27:21 +0100

    added water quality parameters sections

---
+++ texts/water-quality-parameters.js
@@ -0,0 +1,65 @@
+export const waterQualityParameters = `
+  <p>
+    Water quality is quantified by measuring physical, chemical, and biological indicators, often using standardized tests and sensors
+    <ul>
+        <li>Physical parameters: measurable characteristics of water that do not change its chemical composition.
+          <ul class="dash-ul">
+            <li>
+              Temperature: affects dissolved gases and biological activity.
+            </li>
+            <li>
+              Turbidity: measures water clarity; higher turbidity means dirtier water.
+            </li>
+            <li>
+              Color
+            </li>
+            <li>
+              taste and odor
+            </li>
+            <li>
+              Solids
+            </li>
+          </ul>
+
+        </li>
+        <li>Chemical parameters: about chemical properties of the water.
+          <ul class="dash-ul">
+            <li>
+              pH: acidity or alkalinity, with a neutral pH at 7
+            </li>
+            <li>
+              Dissolved oxygen: crucial for aquatic life; warmer water holds lesse dissolved oxygen
+            </li>
+            <li>
+              Nutrients: such as nitrogen and phosphorus, which can indicate pollution levels.
+            </li>
+            <li>
+              Total dissolved solids(TDS): the concentration of all dissolved substances in water.
+            </li>
+            <li>
+              Chlorine
+            </li>
+            <li>
+              Hardness
+            </li>
+            <li>
+              Biological oxygen demand
+            </li>
+          </ul>
+        </li>
+        <li>Biological parameters of water. To asses the presence of living organisms in the water.
+          <ul class="dash-ul">
+            <li>
+              Bacteria, such as E. coli, a common indicator of fexal contamination.
+            </li>
+            <li>
+              Algae: can impact dissolved oxygen levels and are a sign of nutrient enrichment
+            </li>
+            <li>
+              Viruses
+            </li>
+          </ul>
+        </li>
+      </ul>
+  </p>
+`
